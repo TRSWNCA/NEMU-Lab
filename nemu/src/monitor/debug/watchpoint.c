b8c63d1cee6554547a30fc43c5a9852e84863bb9
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-23 21:13:36 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:13:36 up  4:10,  0 users,  load average: 0.03, 0.03, 0.00 e940b1edd7e12a7de17e2ca296f38ca67979227f

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,79 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP * new_wp() {
+  WP * f, * p;
+  f = free_;
+  free_ = free_ -> next;
+  f -> next = NULL;
+  p = head;
+  if (p == NULL)
+    p = head = f;
+  else {
+    while (p -> next != NULL)
+      p = p -> next;
+    p -> next = f;
+  }
+  return f;
+}
 
+void free_wp(WP * wp) {
+  WP * p;
+  p = head;
+  if (head == NULL)
+    Assert(0, "ERROR");
+  else if (p -> NO == wp -> NO) {
+    head = head -> next;
+    p -> next = free_;
+    p -> val = 0;
+    p -> expr[0] = '\0';
+    free_ = p;
+    return;
+  } else {
+    WP * ls = head;
+    p = p -> next;
+    while (p != NULL) {
+      if (p -> NO == wp -> NO) {
+        ls -> next = p -> next;
+        p -> val = 0;
+        p -> next = free_;
+        p -> expr[0] = '\0';
+        free_ = p;
+        return;
+      } else
+        p = p -> next, ls = ls -> next;
+    }
+  }
+}
+
+void delete_wp(int num) {
+  WP * f;
+  f = &wp_pool[num];
+  free_wp(f);
+}
+
+void info_wp() {
+  WP * f;
+  f = head;
+  while (f != NULL) {
+    printf("%d: %s = %d\n", f -> NO, f -> expr, f -> val);
+    f = f -> next;
+  }
+}
+
+bool check_wp() {
+  WP * f;
+  f = head;
+  bool suc, flag = 1;
+  while (f != NULL) {
+    uint32_t ls = expr(f -> expr, &suc);
+    if (!suc)
+      Assert(1, "REEOR\n");
+    if (ls != f -> val) {
+      printf("W %d %s\tOld: %d, New: %d\n", f -> NO, f -> expr, f -> val, ls);
+      flag = 0;
+    }
+    f = f -> next;
+  }
+  return flag;
+}
