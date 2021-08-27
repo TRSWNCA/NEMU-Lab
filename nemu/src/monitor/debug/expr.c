bef2690dad6877c9e769dbfc73b270ebf941a37a
tracer-TRSWNCA <trswnca@yeah.net>
2021-08-27 14:10:01 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:46:31 up 11:00,  0 users,  load average: 0.14, 0.04, 0.01 1ff92747936b4f3f90f91950523932917dafc798

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -212,7 +212,7 @@ uint32_t eval(int l, int r) {
   }
   else {
     int op = dominant_operator(l, r);
-    // printf("op: %d\n", op);
+    Log("op: %d", op);
     if (l == op || tokens[op].type == POINTER || tokens[op].type == MINUS || tokens[op].type == '!') {
       uint32_t ls = eval(l + 1, r);
       switch (tokens[l].type) {
