2e0954954b4e0e30de864fa9d9511fd039cd110c
tracer-TRSWNCA <trswnca@yeah.net>
2021-08-27 13:21:13 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:43:52 up 10:57,  0 users,  load average: 0.00, 0.00, 0.00 706fc0b69fa6ce207496457c77314e1aad5fdd8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -83,7 +83,7 @@ static bool make_token(char *e) {
         int substr_len = pmatch.rm_eo;
 
         char *ls = e + position + 1;
-        //Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
