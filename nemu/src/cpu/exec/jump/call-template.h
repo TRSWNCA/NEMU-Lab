72c0fabe77f3ecb75cdbbfae8f7132e9997ab0ad
tracer-TRSWNCA <trswnca@yeah.net>
2021-08-30 10:16:13 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:57:06 up  3:41,  0 users,  load average: 0.15, 0.03, 0.01 ace793bb6cf22b77b211f461e2b0948e16995d1f

---
+++ nemu/src/cpu/exec/jump/call-template.h
@@ -0,0 +1,23 @@
+#include "cpu/exec/template-start.h"
+
+#define instr call
+make_helper(concat(call_i_, SUFFIX)) {
+  int len = concat(decode_i_, SUFFIX)(eip + 1);
+  reg_l(R_ESP) -= DATA_BYTE;
+  swaddr_write(reg_l(R_ESP), 4, cpu.eip + len);
+  DATA_TYPE_S lsval = op_src -> val;
+  print_asm("call: %x", cpu.eip + 1 + len + lsval);
+  cpu.eip += lsval;
+  return len + 1;
+}
+make_helper(concat(call_rm_, SUFFIX)) {
+  int len = concat(decode_rm_, SUFFIX)(eip + 1);
+  reg_l(R_ESP) -= DATA_BYTE;
+  swaddr_write(reg_l(R_ESP), 4, cpu.eip + len);
+  DATA_TYPE_S lsval = op_src -> val;
+  print_asm("call: %x", lsval);
+  cpu.eip = lsval - len - 1;
+  return len + 1;
+}
+
+#include "cpu/exec/template-end.h"
