cab68939941ad5f255b182dea3ae86f935f9f112
tracer-TRSWNCA <trswnca@yeah.net>
2021-08-31 14:06:03 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:30:28 up  1:15,  0 users,  load average: 0.21, 0.05, 0.02 82c3d2d519dac9b83291c300791c99194287f316

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -95,7 +95,7 @@ bool check_wp() {
     if (!suc)
       Assert(1, "REEOR\n");
     if (f -> check_eval) {
-      if (ls != f -> eval) {
+      if (ls == f -> eval) {
         printf("Hint watchpoint %d at 0x%x:\n", f -> NO, cpu.eip);
         printf("value: %d (0x%x)\n", ls, ls);
         flag = 0;
