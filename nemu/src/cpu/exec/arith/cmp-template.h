5bb2b52ece5b676f1037c1b7fe9389d32c13d4de
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-01 15:26:58 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:26:58 up  4:56,  0 users,  load average: 0.01, 0.02, 0.00 5be9d642c603ca5d13daf5e61668b1823d8ae61e

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -3,11 +3,20 @@
 #define instr cmp
 
 static void do_execute() {
-  DATA_TYPE result = op_dest -> val - op_src -> val;
-  update_eflags_pf_zf_sf(result);
-  cpu.eflags.CF = op_src -> val < op_dest -> val;
-	cpu.eflags.OF = MSB(~(op_dest-> val ^ op_src -> val) & (op_dest -> val ^ result));
-  print_asm_template2();
+	DATA_TYPE result = op_dest->val - op_src->val;
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.eflags.CF = op_dest->val < op_src->val;
+	cpu.eflags.SF=result >> len;
+    	int s1,s2;
+	s1=op_dest->val>>len;
+	s2=op_src->val>>len;
+    	cpu.eflags.OF=(s1 != s2 && s2 == cpu.eflags.SF) ;
+    	cpu.eflags.ZF=!result;
+	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.eflags.PF=!(result & 1);
+	print_asm_template2();
 }
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
