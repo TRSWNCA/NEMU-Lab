be6a56a73636c4b5e16b0224415a87a43f207e28
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-01 09:39:18 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:39:18 up  1:22,  0 users,  load average: 0.00, 0.00, 0.00 ac1e646bc5c5aa95c7240eb22d1f46cfd61d3596

--- nemu/src/cpu/exec/arith/cmp.c
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -1,17 +1,19 @@
 #include "cpu/exec/helper.h"
 
 #define DATA_BYTE 1
-#include "je-template.h"
+#include "cmp-template.h"
 #undef DATA_BYTE
 
 #define DATA_BYTE 2
-#include "je-template.h"
+#include "cmp-template.h"
 #undef DATA_BYTE
 
 #define DATA_BYTE 4
-#include "je-template.h"
+#include "cmp-template.h"
 #undef DATA_BYTE
 
 /* for instruction encoding overloading */
-
-make_helper_v(je_i)
+make_helper_v(cmp_i2a)
+make_helper_v(cmp_i2rm)
+make_helper_v(cmp_r2rm)
+make_helper_v(cmp_si2rm)
