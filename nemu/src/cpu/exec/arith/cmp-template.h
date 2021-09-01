9ec2b369228bfe1bae76e14f8f735330f2def912
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-01 15:21:08 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:21:08 up  4:50,  0 users,  load average: 0.02, 0.02, 0.00 d638af589477f9e75c6fe97297064615f774cba0

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -4,18 +4,9 @@
 
 static void do_execute() {
   DATA_TYPE result = op_dest -> val - op_src -> val;
-  int len = (DATA_BYTE << 3) - 1;
-  cpu.eflags.CF = op_dest -> val < op_src -> val;
-  cpu.eflags.SF = result >> len;
-  int s1, s2;
-  s1 = op_dest -> val >> len;
-  s2 = op_src -> val >> len;
-  cpu.eflags.OF = (s1 != s2 && s2 == cpu.eflags.SF);
-  cpu.eflags.ZF = !result;
-  result ^= result >> 4;
-  result ^= result >> 2;
-  result ^= result >> 1;
-  cpu.eflags.PF = !(result & 1);
+  update_eflags_pf_zf_sf(result);
+  cpu.eflags.CF = op_src -> val < op_dest -> val;
+	cpu.eflags.OF = MSB(~(op_dest-> val ^ op_src -> val) & (op_dest -> val ^ result));
   print_asm_template2();
 }
 
@@ -26,5 +17,6 @@ make_instr_helper(si2rm)
 make_instr_helper(i2a)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
+make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
