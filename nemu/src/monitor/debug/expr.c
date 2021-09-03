330d6c0df7d6e492a3e59e4a78656cbf7438c05f
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 15:27:31 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:27:31 up  2:26,  0 users,  load average: 0.08, 0.04, 0.00 2c499429933723505eb4a0fa29536b7545865bbf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -170,7 +170,7 @@ int dominant_operator(int l, int r) {
 }
 
 uint32_t eval(int l, int r, bool *succuess) {
-  //Log("%d %d", l, r);
+  Log("%d %d", l, r);
   if (l > r) {
     Assert(l > r, "ERROR\n");
     return 0;
