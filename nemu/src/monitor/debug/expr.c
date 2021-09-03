6329081dc25cbfb53a28870ee7369d8e9241a813
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 15:32:00 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:32:00 up  2:31,  0 users,  load average: 0.00, 0.01, 0.00 40942a226ffe5e0cdbf4b6c713cb439344701367

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -274,6 +274,7 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
+  Log("%d", REGISTER);
   Log("%d", nr_token);
 
   /* TODO: Insert codes to evaluate the expression. */
