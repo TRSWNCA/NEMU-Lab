1685f4c1e2f69b70b2078769f73ef05ccc9d46ba
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-03 13:07:19 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:13:23 up  4:42,  0 users,  load average: 0.08, 0.04, 0.00 9db29e55a61d61146eaf8272cab9b6fd68fca0ed

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -20,17 +20,17 @@ static make_helper(_2byte_esc);
 	
 /* 0x80 */
 make_group(group1_b,
-	add_i2rm_b, inv, inv, inv, 
+	add_i2rm_b, inv, adc_i2rm_b, inv, 
 	and_i2rm_b, inv, inv, inv)
 
 /* 0x81 */
 make_group(group1_v,
-	add_i2rm_v, inv, inv, inv, 
+	add_i2rm_v, inv, adc_i2rm_v, inv, 
 	and_i2rm_v, sub_i2rm_v, inv, inv)
 
 /* 0x83 */
 make_group(group1_sx_v,
-	inv, or_si2rm_v, inv, inv, 
+	add_si2rm_v, or_si2rm_v, adc_si2rm_v, inv, 
 	and_si2rm_v, sub_si2rm_v, inv, cmp_si2rm_v)
 
 /* 0xc0 */
