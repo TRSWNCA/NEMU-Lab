0d7896f763072f8edbd0b45d3bb1fe6204129958
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 14:14:12 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:14:12 up  1:13,  0 users,  load average: 0.14, 0.07, 0.01 953600f4eb8c367348b1426bcf6d7301261641a4

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -2,25 +2,31 @@
 
 #define instr adc
 
-static void do_execute () {
-	DATA_TYPE result = op_dest->val + op_src->val + cpu.eflags.CF;
-	OPERAND_W(op_dest, result);
-
-	/* TODO: Update EFLAGS. */
-	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-	cpu.eflags.CF = result < op_dest->val;
-	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
-
+void do_execute() {
+	DATA_TYPE res = op_dest->val + op_src->val + cpu.eflags.CF;
+	int len = (DATA_BYTE << 3) - 1;
+	int s1, s2;
+	cpu.eflags.CF = (res < op_dest->val);
+	cpu.eflags.SF = res >> len;
+	s1 = op_dest->val >> len;
+	s2 = op_src->val >> len;
+	cpu.eflags.OF = (s1 == s2 && s1 != cpu.eflags.SF);
+	cpu.eflags.ZF = !res;
+	OPERAND_W(op_dest, res);
+	res ^= res >> 4;
+	res ^= res >> 2;
+	res ^= res >> 1;
+	cpu.eflags.PF = !(res & 1);
 	print_asm_template2();
 }
+
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+make_instr_helper(i2a)
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(si2rm)
 #endif
-
-make_instr_helper(i2a)
-make_instr_helper(i2rm)
-make_instr_helper(r2rm)
-make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
