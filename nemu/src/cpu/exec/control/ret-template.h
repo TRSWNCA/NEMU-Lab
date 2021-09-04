d0680de6079e0b8caab06858540cbfe3d2e2b526
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 16:50:38 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:50:38 up  5:55,  0 users,  load average: 0.00, 0.09, 0.08 a831d7a03311b4629e21178827065d05eb5463e

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -3,7 +3,7 @@
 #define instr ret
 
 make_helper(concat(ret_n_, SUFFIX)) {
-  cpu.eip = MEM_R(reg_l(R_ESP));// - 1;
+  cpu.eip = MEM_R(reg_l(R_ESP)) - 1;
   reg_l(R_ESP) += DATA_BYTE;
   print_asm("ret");
   return 1;
@@ -11,11 +11,11 @@ make_helper(concat(ret_n_, SUFFIX)) {
 
 make_helper(concat(ret_i_, SUFFIX)) {
   uint32_t num = instr_fetch(eip + 1, 2);
-  cpu.eip = MEM_R(reg_l(R_ESP));
+  cpu.eip = MEM_R(reg_l(R_ESP)) - 3;
   reg_l(R_ESP) += DATA_BYTE;
   reg_l(R_ESP) += num;
   print_asm("ret 0x%x", num);
-  return 1;
+  return 3;
 }
 
 #include "cpu/exec/template-end.h"
