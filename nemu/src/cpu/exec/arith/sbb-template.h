0230c4b5dd803ab268839dce57b3f9589d70d1e8
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 16:34:03 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:34:03 up  5:38,  0 users,  load average: 0.18, 0.09, 0.02 440ad6389e5d1a7b3b576e2214b95c0cde5d431

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -7,13 +7,32 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-	cpu.eflags.CF = result < op_dest->val;
+  update_eflags_pf_zf_sf(result);
+	int len = (DATA_BYTE << 3) - 1;
+  /*
+	cpu.eflags.SF = ans >> len;
+	cpu.eflags.ZF = !ans;
+	ans ^= ans >> 4;
+	ans ^= ans >> 2;
+	ans ^= ans >> 1;
+	cpu.eflags.PF = !(ans & 1);
+	cpu.eflags.CF = op_dest->val < ls;
 	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+  */
+	cpu.eflags.OF = ((op_dest->val >> len) != (result >> len) && (result >> len) == cpu.eflags.SF);
+
+	cpu.eflags.CF = result > op_dest->val;
 
 	print_asm_template2();
 }
 
+make_instr_helper(i2rm)
 make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+make_instr_helper(i2a)
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
 
 #include "cpu/exec/template-end.h"
