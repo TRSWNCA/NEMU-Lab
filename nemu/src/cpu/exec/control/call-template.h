05b3f064cf3e58aae3efa51d9dc58a46fbaf6bc0
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-04 12:26:10 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:19:35 up  1:18,  0 users,  load average: 0.00, 0.02, 0.00 7296ba4c34cb384976dd82bfa4150894d47cb15

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -1,25 +1,23 @@
 #include "cpu/exec/template-start.h"
 
 #define instr call
+
 make_helper(concat(call_i_, SUFFIX)) {
-  int len = concat(decode_i_, SUFFIX)(eip + 1);
-  //Log("%d", len);
-  reg_l(R_ESP) -= DATA_BYTE;
-  swaddr_write(reg_l(R_ESP), 4, cpu.eip + len);
-  DATA_TYPE_S lsval = op_src -> val;
-  print_asm("call: %x", cpu.eip + 1 + len + lsval);
-  cpu.eip += lsval;
-  return len + 1;
+	int len = concat(decode_i_, SUFFIX)(eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	MEM_W(reg_l(R_ESP), cpu.eip + len + 1);
+	cpu.eip += (DATA_TYPE_S)op_src->val;
+	print_asm("call: 0x%x", cpu.eip + len + 1);
+	return len + 1;
 }
 
 make_helper(concat(call_rm_, SUFFIX)) {
-  int len = concat(decode_rm_, SUFFIX)(eip + 1);
-  reg_l(R_ESP) -= DATA_BYTE;
-  swaddr_write(reg_l(R_ESP), 4, cpu.eip + len);
-  DATA_TYPE_S lsval = op_src -> val;
-  print_asm("call: %x", lsval);
-  cpu.eip = lsval - len - 1;
-  return len + 1;
+	int len = concat(decode_rm_, SUFFIX)(eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	MEM_W(reg_l(R_ESP), cpu.eip + len + 1);
+	cpu.eip = (DATA_TYPE_S)op_src->val - len - 1;
+	print_asm("call: %s", op_src->str);
+	return len + 1;
 }
 
 #include "cpu/exec/template-end.h"
