311ba42f2456edfa2dc3ed17b656d9fba9cf2dd5
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 17:00:29 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:00:29 up  6:05,  0 users,  load average: 0.17, 0.08, 0.07 bae80acdd11d694267e01da7d063796d7c626390

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -9,21 +9,6 @@ void do_execute() {
 	cpu.eflags.CF = result < op_dest->val;
 	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
   print_asm_template2();
-  /*
-  int len = (DATA_BYTE << 3) - 1, s1, s2;
-  cpu.eflags.CF = (res < op_dest -> val);
-  cpu.eflags.SF = res >> len;
-  s1 = op_dest -> val >> len;
-  s2 = op_src -> val >> len;
-  cpu.eflags.OF = (s1 == s2 && s1 != cpu.eflags.SF);
-  cpu.eflags.ZF = !res;
-  OPERAND_W(op_dest, res);
-  res ^= res >> 4;
-  res ^= res >> 2;
-  res ^= res >> 1;
-  cpu.eflags.PF = !(res & 1);
-  print_asm_template2();
-  */
 }
 
 make_instr_helper(i2a)
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -81,7 +81,7 @@ make_group(group4,
 /* 0xff */
 make_group(group5,
 	inv, dec_rm_v, inv, inv, 
-	jmp_rm_l, inv, inv, inv)
+	jmp_rm_l, inv, push_rm_v, inv)
 
 make_group(group6,
 	inv, inv, inv, inv, 
