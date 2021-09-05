724c719ec71fd08a65b48efac89db26ecc0f5240
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-05 13:36:04 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:36:04 up  3:26,  0 users,  load average: 0.16, 0.07, 0.04 2fd9d842d068bbf1fc59722372334444f8d18a9b

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -7,7 +7,7 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-	cpu.eflags.CF = result > op_dest->val;
+	cpu.eflags.CF = op_src->val < op_dest->val;
 	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
 
 	print_asm_template2();
