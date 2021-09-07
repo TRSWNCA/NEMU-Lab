5d019b9148b27fffb15ff69c9018a562841eb46f
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 21:08:20 +0800

    >  run 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:08:20 up  9:03,  0 users,  load average: 0.23, 0.22, 0.09 fff69282fedda1a42724bbd85bcabb5b214d989

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,49 +1,71 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  return (FLOAT)(((long long)a * (long long)b) >> 16);
+	long long result =1ll * a * b;
+	result = (FLOAT)(result >> 16);
+	return result;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  /* Dividing two 64-bit integers needs the support of another library
-   * `libgcc', other than newlib. It is a dirty work to port `libgcc'
-   * to NEMU. In fact, it is unnecessary to perform a "64/64" division
-   * here. A "64/32" division is enough.
-   *
-   * To perform a "64/32" division, you can use the x86 instruction
-   * `div' or `idiv' by inline assembly. We provide a template for you
-   * to prevent you from uncessary details.
-   *
-   *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
-   *
-   * If you want to use the template above, you should fill the "???"
-   * correctly. For more information, please read the i386 manual for
-   * division instructions, and search the Internet about "inline assembly".
-   * It is OK not to use the template above, but you should figure
-   * out another way to perform the division.
-   */
+	/* Dividing two 64-bit integers needs the support of another library
+	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
+	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
+	 * here. A "64/32" division is enough.
+	 *
+	 * To perform a "64/32" division, you can use the x86 instruction
+	 * `div' or `idiv' by inline assembly. We provide a template for you
+	 * to prevent you from uncessary details.
+	 *
+	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
+	 *
+	 * If you want to use the template above, you should fill the "???"
+	 * correctly. For more information, please read the i386 manual for
+	 * division instructions, and search the Internet about "inline assembly".
+	 * It is OK not to use the template above, but you should figure
+	 * out another way to perform the division.
+	 */
 
-  int sign = 1;
-  if (a < 0)
-    sign = -sign, a = -a;
-  if (b < 0)
-    sign = -sign, b = -b;
-  int res = a / b;
-  a = a % b;
-  int i;
-  for (i = 0; i < 16; i++) {
-    a <<= 1;
-    res <<= 1;
-    if (a >= b) {
-      a = a - b;
-      res++;
-    }
-  }
-  return res * sign;
+	int op = 1;
+	if((a < 0) == 1)
+	{
+		op *= -1;
+		a = -a;
+	}
+	if((b < 0) == 1)
+	{
+		op *= -1;
+		b = -b;
+	}
+	int result = a / b;
+	a %= b;
+
+	int i;
+	for(i = 0 ; i < 16 ; i++)
+	{
+		a <<= 1;
+		result <<= 1;
+		if(a >= b)
+		{
+			a -= b;
+			result |= 1;
+		}
+	}
+	return op * result;
+
 }
 
 FLOAT f2F(float a) {
-  	int b = *(int *)&a;
+	/* You should figure out how to convert `a' into FLOAT without
+	 * introducing x87 floating point instructions. Else you can
+	 * not run this code in NEMU before implementing x87 floating
+	 * point instructions, which is contrary to our expectation.
+	 *
+	 * Hint: The bit representation of `a' is already on the
+	 * stack. How do you retrieve it to another variable without
+	 * performing arithmetic operations on it directly?
+	 */
+
+	int b = *(int *)&a;
 	int sign = b >> 31;
 	int exp = (b >> 23) & 0xff;
 	int c = b & 0x7fffff;
@@ -66,59 +88,39 @@ FLOAT f2F(float a) {
 		return -c;
 	else
 		return c;
-  /* You should figure out how to convert `a' into FLOAT without
-   * introducing x87 floating point instructions. Else you can
-   * not run this code in NEMU before implementing x87 floating
-   * point instructions, which is contrary to our expectation.
-   *
-   * Hint: The bit representation of `a' is already on the
-   * stack. How do you retrieve it to another variable without
-   * performing arithmetic operations on it directly?
-   */
-
-  /*
-  int b = *(int*)&a;
-  int sign = b >> 31;
-  int exp = (b >> 23) & 0xff;
-  int x = b & 0x7fffff;
-  if (exp != 0)
-    x += 1 << 23;
-  exp -= 150;
-  if (exp < -16)
-    x >>= -16 - exp;
-  if (exp > -16)
-    x <<= 16 + exp;
-  return sign == 0 ? x : -x;
-  */
 }
 
 FLOAT Fabs(FLOAT a) {
-  return a < 0 ? -a : a;
+	if(a < 0)
+		return -a;
+	else
+		return a;
 }
 
 /* Functions below are already implemented */
 
 FLOAT sqrt(FLOAT x) {
-  FLOAT dt, t = int2F(2);
+	FLOAT dt, t = int2F(2);
 
-  do {
-    dt = F_div_int((F_div_F(x, t) - t), 2);
-    t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+	do {
+		dt = F_div_int((F_div_F(x, t) - t), 2);
+		t += dt;
+	} while(Fabs(dt) > f2F(1e-4));
 
-  return t;
+	return t;
 }
 
 FLOAT pow(FLOAT x, FLOAT y) {
-  /* we only compute x^0.333 */
-  FLOAT t2, dt, t = int2F(2);
+	/* we only compute x^0.333 */
+	FLOAT t2, dt, t = int2F(2);
 
-  do {
-    t2 = F_mul_F(t, t);
-    dt = (F_div_F(x, t2) - t) / 3;
-    t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+	do {
+		t2 = F_mul_F(t, t);
+		dt = (F_div_F(x, t2) - t) / 3;
+		t += dt;
+	} while(Fabs(dt) > f2F(1e-4));
 
-  return t;
+	return t;
 }
+
 
