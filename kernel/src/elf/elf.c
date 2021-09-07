fab587b96c1c732c669e0a7007f15049773afc26
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 22:07:44 +0800

    >  compile kernel 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:07:44 up 10:02,  0 users,  load average: 0.00, 0.00, 0.00 1c97b52b78853c584a763f6d8c7d93ca7220fc76

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -33,9 +33,9 @@ uint32_t loader() {
 
   /* TODO: fix the magic number with the correct one */
   const uint32_t elf_magic = 0x464c457f;
-  set_bp();
   uint32_t* p_magic = (void*)buf;
   nemu_assert(*p_magic == elf_magic);
+  set_bp();
 
   /* Load each program segment */
   int i;
