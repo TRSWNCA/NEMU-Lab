c2f34f701e7afecb965b19496f954edd947fc909
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 14:51:34 +0800

    >  run 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:51:34 up  6:44,  0 users,  load average: 0.14, 0.05, 0.01 7174ae0d405723e1b42d452baa838ae71b4c00a

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -4,21 +4,18 @@
 /* solve 0.5x^2 + 0.5x - 0.5 = 0 */
 
 int main() {
-	FLOAT a = f2F(0.5);
-	FLOAT b = f2F(0.5);
-	FLOAT c = f2F(-0.5);
-  set_bp();
+	FLOAT a = f2F((unsigned int)(0.5));
+	FLOAT b = f2F((unsigned int)(0.5));
+	FLOAT c = f2F((unsigned int)(-0.5));
 
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
-  set_bp();
 
 	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
 	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
-  set_bp();
 
-	FLOAT x1_ans = f2F(0.618);
-	FLOAT x2_ans = f2F(-1.618);
+	FLOAT x1_ans = f2F((unsigned int)(0.618));
+	FLOAT x2_ans = f2F((unsigned int)(-1.618));
   set_bp();
 
 	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
