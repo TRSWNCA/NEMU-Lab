7f1a0d24cb35811739f96e7bb639de8bc2a11f73
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-07 12:48:18 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:56:17 up  6:01,  0 users,  load average: 0.10, 0.06, 0.07 b9104b714c0d4b8aa10aa867389b0e058b82ad2b

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -3,7 +3,13 @@
 #define instr add
 
 void do_execute() {
-  DATA_TYPE res = op_dest -> val + op_src -> val;
+  DATA_TYPE result = op_dest -> val + op_src -> val;
+  OPERAND_W(op_dest, result);
+  update_eflags_pf_zf_sf(result);
+	cpu.eflags.CF = result < op_dest->val;
+	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+  print_asm_template2();
+  /*
   int len = (DATA_BYTE << 3) - 1, s1, s2;
   cpu.eflags.CF = (res < op_dest -> val);
   cpu.eflags.SF = res >> len;
@@ -17,6 +23,7 @@ void do_execute() {
   res ^= res >> 1;
   cpu.eflags.PF = !(res & 1);
   print_asm_template2();
+  */
 }
 
 make_instr_helper(i2a)
