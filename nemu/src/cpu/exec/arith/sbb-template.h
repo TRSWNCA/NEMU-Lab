6ab732fdf6e8ba56bea695fb21f56710d7d75d4a
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-09 21:00:31 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:00:31 up 17:42,  0 users,  load average: 0.15, 0.23, 0.17 697557fe7ac5ea2a72432cd1e52320eec3bd3ec1

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -5,22 +5,8 @@
 static void do_execute () {
 	DATA_TYPE result = op_dest->val - (op_src->val + cpu.eflags.CF);
 	OPERAND_W(op_dest, result);
-
-	/* TODO: Update EFLAGS. */
   update_eflags_pf_zf_sf(result);
-	int len = (DATA_BYTE << 3) - 1;
-  /*
-	cpu.eflags.SF = ans >> len;
-	cpu.eflags.ZF = !ans;
-	ans ^= ans >> 4;
-	ans ^= ans >> 2;
-	ans ^= ans >> 1;
-	cpu.eflags.PF = !(ans & 1);
-	cpu.eflags.CF = op_dest->val < ls;
-	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
-  */
-	cpu.eflags.OF = ((op_dest->val >> len) != (result >> len) && (result >> len) == cpu.eflags.SF);
-
+  cpu.eflags.OF = MSB((op_dest -> val ^ op_src -> val) & (~(op_src -> val ^ result)));
 	cpu.eflags.CF = result > op_dest->val;
 
 	print_asm_template2();
