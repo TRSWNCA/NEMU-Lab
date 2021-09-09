632c292a31f5a2c9cfdb3cbe6cda81cefcf19ae0
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-09 20:37:17 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:37:17 up 17:19,  0 users,  load average: 0.00, 0.02, 0.01 2fe0753596f38065bd03e67a1ef11d8894311711

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -5,7 +5,7 @@
 static void do_execute() {
 	DATA_TYPE result = op_dest->val - op_src->val;
 	int len = (DATA_BYTE << 3) - 1;
-  update_eflags_pf_zf_sf(result);
+  update_eflags_pf_zf_sf((DATA_TYPE_S)result);
 	cpu.eflags.CF = op_dest->val < op_src->val;
 	int s1, s2;
 	s1 = op_dest->val >> len;
