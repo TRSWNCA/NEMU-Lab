e568f29ad19cea6295ef78c469ceca9b3653c280
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-12 10:31:24 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:23:21 up  3:14,  0 users,  load average: 0.15, 0.07, 0.01 9342bac525280367653154888fa2b37d3fdcf907

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -2,18 +2,18 @@
 
 #define instr jmp
 
-static void do_execute() {
-	cpu.eip += op_src->val;
-	print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
+make_helper(concat(jmp_i_, SUFFIX)) {
+  int len = concat(decode_i_, SUFFIX)(eip + 1);
+  cpu.eip += (DATA_TYPE_S)op_src->val;
+  print_asm("jmp: 0x%x", cpu.eip + len + 1);
+  return len + 1;
 }
 
-make_instr_helper(si)
-#if DATA_BYTE == 4
-make_helper(jmp_rm_l) {
-	int len = decode_rm_l(eip + 1);
-	cpu.eip = op_src->val - (len + 1);
-	print_asm(str(instr) " *%s", op_src->str);
-	return len + 1;
+make_helper(concat(jmp_rm_, SUFFIX)) {
+  int len = concat(decode_rm_, SUFFIX)(eip + 1);
+  cpu.eip = (DATA_TYPE_S)op_src->val - len - 1;
+  print_asm("jmp: %s", op_src->str);
+  return len + 1;
 }
-#endif
+
 #include "cpu/exec/template-end.h"
