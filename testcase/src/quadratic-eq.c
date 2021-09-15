68c1465835d82253bd12a2a36c597ea97484e076
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-15 11:13:35 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:24:41 up  5:45,  0 users,  load average: 0.10, 0.07, 0.03 3b46237fb3fbeeb87f1d6295ae31a64b3989d894

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -7,15 +7,19 @@ int main() {
 	FLOAT a = f2F(0.5);
 	FLOAT b = f2F(0.5);
 	FLOAT c = f2F(-0.5);
+  set_bp();
 
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
+  set_bp();
 
 	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
 	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
+  set_bp();
 
 	FLOAT x1_ans = f2F(0.618);
 	FLOAT x2_ans = f2F(-1.618);
+  set_bp();
 
 	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
 	nemu_assert(Fabs(x2_ans - x2) < f2F(1e-4));
