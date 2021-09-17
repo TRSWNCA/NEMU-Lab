2f0f0f07f384defd99e393acade5e53e4f7a8c47
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-17 13:20:34 +0806

    >  compile kernel 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:11:57 up 10:07,  0 users,  load average: 0.07, 0.02, 0.00 39c086ee2fde5a7bd99b66c8fe6455787da03358

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -1,4 +1,3 @@
-#include "trap.h"
 #include "common.h"
 #include "memory.h"
 #include <string.h>
@@ -18,66 +17,63 @@ void create_video_mapping();
 uint32_t get_ucr3();
 
 uint32_t loader() {
-  Elf32_Ehdr* elf;
-  Elf32_Phdr* ph = NULL;
+	Elf32_Ehdr *elf;
+	Elf32_Phdr *ph = NULL;
 
-  uint8_t buf[4096];
+	uint8_t buf[4096];
 
 #ifdef HAS_DEVICE
-  ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
+	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #else
-  ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
+	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #endif
 
-  elf = (void*)buf;
+	elf = (void*)buf;
 
-  /* TODO: fix the magic number with the correct one */
-  const uint32_t elf_magic = 0x464c457f;
-  uint32_t* p_magic = (void*)buf;
-  nemu_assert(*p_magic == elf_magic);
+	/* TODO: fix the magic number with the correct one */
+	const uint32_t elf_magic = 0x464c457f;
+	uint32_t *p_magic = (void *)buf;
+	nemu_assert(*p_magic == elf_magic);
 
-  /* Load each program segment */
-  int i;
-  ph = (void*)(buf + elf->e_phoff);
-  // panic("please implement me");
-  for (i = 0; i < elf->e_phnum; i++, ph++) {
-    /* Scan the program header table, load each segment into memory */
-    if (ph->p_type == PT_LOAD) {
-    set_bp();
+	/* Load each program segment */
+	int i;
+	ph = (void *)(buf + elf->e_phoff);
+	// panic("please implement me");
+	for(i = 0; i < elf->e_phnum; i++, ph++) {
+		/* Scan the program header table, load each segment into memory */
+		if(ph->p_type == PT_LOAD) {
 
-      /* TODO: read the content of the segment from the ELF file
-       * to the memory region [VirtAddr, VirtAddr + FileSiz)
-       */
-      ramdisk_read((void*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
-
-      /* TODO: zero the memory region
-       * [VirtAddr + FileSiz, VirtAddr + MemSiz)
-       */
-      memset((void*)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			/* TODO: read the content of the segment from the ELF file 
+			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
+			 */
+			ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
+			 
+			/* TODO: zero the memory region 
+			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
+			 */
+			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 
 #ifdef IA32_PAGE
-      /* Record the program break for future use. */
-      extern uint32_t cur_brk, max_brk;
-      uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-      if (cur_brk < new_brk) {
-        max_brk = cur_brk = new_brk;
-      }
+			/* Record the program break for future use. */
+			extern uint32_t cur_brk, max_brk;
+			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
+			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
 #endif
-    }
-  }
+		}
+	}
 
-  volatile uint32_t entry = elf->e_entry;
+	volatile uint32_t entry = elf->e_entry;
 
 #ifdef IA32_PAGE
-  mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
+	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
 
 #ifdef HAS_DEVICE
-  create_video_mapping();
+	create_video_mapping();
 #endif
 
-  write_cr3(get_ucr3());
+	write_cr3(get_ucr3());
 #endif
 
-  return entry;
+	return entry;
 }
