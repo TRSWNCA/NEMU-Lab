0c4d783b8509133ec1a7cbac8401df36ad5a8029
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-17 13:00:22 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:38:32 up 13:51,  0 users,  load average: 0.35, 0.17, 0.07 4d53895f735d5ba289d54a6d31c2fd134c268b04

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -188,7 +188,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x58 */	inv, inv, inv, inv, 
 /* 0x5c */	inv, inv, inv, inv, 
 /* 0x60 */	inv, inv, inv, inv,
-/* 0x64 */	inv, inv, inv, inv,
+/* 0x64 */	inv, inv, inv, movzw_l,
 /* 0x68 */	inv, inv, inv, inv, 
 /* 0x6c */	inv, inv, inv, inv, 
 /* 0x70 */	inv, inv, inv, inv,
