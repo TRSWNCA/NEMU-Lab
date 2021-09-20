477815ae25c90be842c5ec76d9985b9202c40eb8
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-20 10:12:23 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:59:24 up 17:41,  0 users,  load average: 0.03, 0.21, 0.15 a74dffb8d312a8ff4479b8b920e9aeec90290e

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -30,19 +30,12 @@ make_instr_helper(si2rm)
 #define instr sub
 
 void do_execute() {
-  DATA_TYPE res = op_dest->val - op_src->val;
-  OPERAND_W(op_dest, res);
-  int len = (DATA_BYTE << 3) - 1;
-  cpu.eflags.SF = res >> len;
-  cpu.eflags.ZF = !res;
-  res ^= res >> 4;
-  res ^= res >> 2;
-  res ^= res >> 1;
-  cpu.eflags.PF = !(res & 1);
-
-  cpu.eflags.CF = op_dest->val < op_src->val;
-  cpu.eflags.OF = ((op_dest->val >> len) != (op_src->val >> len) && (op_src->val >> len) == cpu.eflags.SF);
-  print_asm_template2();
+	DATA_TYPE result = op_dest->val - op_src->val;
+  OPERAND_W(op_dest, result);
+  update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = op_dest->val < op_src->val;
+  cpu.eflags.OF = MSB((op_dest -> val ^ op_src -> val) & (~(op_src -> val ^ result)));
+	print_asm_template2();
 }
 
 make_instr_helper(i2a)
