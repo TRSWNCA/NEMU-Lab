980745f8d897aa9cefcbec3b934b9a98c8d1aab7
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-20 13:37:14 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:57:27 up  9:02,  0 users,  load average: 0.00, 0.02, 0.00 f74f44ee8aa7a9fd753ff34cad64ae56d4c7e6f7

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,7 +12,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
   int cache_L1_way_1_index = read_cache_L1(addr);
   uint32_t block_bias = addr & (CACHE_BLOCK_SIZE - 1);
   uint8_t ret[BURST_LEN << 1];
-  printf("%d\n", block_bias);
+  //printf("%d\n", block_bias);
   if (block_bias + len > CACHE_BLOCK_SIZE) {
     int cache_L1_way_2_index = read_cache_L1(addr + CACHE_BLOCK_SIZE - block_bias);
     memcpy(ret, cache_L1[cache_L1_way_1_index].data + block_bias, CACHE_BLOCK_SIZE - block_bias);
@@ -22,9 +22,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
   }
   int tmp = 0;
   uint32_t ans = unalign_rw(ret + tmp, 4) & (~0u >> ((4 - len) << 3));
-  //Log("%d", ans);
   return ans;
-  // return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
