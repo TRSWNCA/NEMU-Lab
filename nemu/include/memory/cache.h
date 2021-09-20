dae31503a581769c8971a6f866d17df89306c574
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-20 11:32:57 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:54:09 up  8:59,  0 users,  load average: 0.11, 0.04, 0.01 a1680f0e429cca61896c4650c568bf269c606e84

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -3,59 +3,49 @@
 
 #include "common.h"
 
-#define Cache_Block_Bit 6
-#define Cache_Block_Size 64
-#define Cache_L1_Cap 64 * 1024
-#define Cache_L1_Way_Bit 3
-#define Cache_L1_Way_Num 8
-#define Cache_L1_Set_Bit 7
-#define Cache_L1_Set_Num 128
+#define CACHE_BLOCK_BIT 6
+#define CACHE_BLOCK_SIZE 64
+#define CACHE_L1_CAP 64 * 1024
+#define CACHE_L1_WAY_BIT 3
+#define CACHE_L1_WAY_NUM 8
+#define CACHE_L1_SET_BIT 7
+#define CACHE_L1_SET_NUM 128
+
+#define CACHE_L2_CAP 4 * 1024 * 1024
+#define CACHE_L2_WAY_BIT 4
+#define CACHE_L2_WAY_NUM 16
+#define CACHE_L2_SET_BIT 12
+#define CACHE_L2_SET_NUM 4096
 
 typedef struct{
-    uint8_t data[Cache_Block_Size];
+    uint8_t data[CACHE_BLOCK_SIZE];
     uint32_t tag;
     bool valid_value;
-}Cache_L1_Way;
+} L1;
 
-Cache_L1_Way cache_L1[Cache_L1_Set_Num * Cache_L1_Way_Num];
+L1 cache_L1[CACHE_L1_SET_NUM * CACHE_L1_WAY_NUM];
 
 void init_cache();
 
-int read_Cache_L1(hwaddr_t);
+int read_cache_L1(hwaddr_t);
 
-void write_Cache_L1(hwaddr_t, size_t, uint32_t);
+void write_cache_L1(hwaddr_t, size_t, uint32_t);
 
-/*
-Cache L2
-cache block 存储空间的大小为 64B
-cache 存储空间的大小为 4MB
-16-way set associative
-标志位包括 valid bit 和 dirty bit
-替换算法采用随机方式
-write back
-write allocate
-*/
-
-#define Cache_L2_Cap 4 * 1024 * 1024
-#define Cache_L2_Way_Bit 4
-#define Cache_L2_Way_Num 16
-#define Cache_L2_Set_Bit 12
-#define Cache_L2_Set_Num 4096
 
 typedef struct{
-    uint8_t data[Cache_Block_Size];
+    uint8_t data[CACHE_BLOCK_SIZE];
     uint32_t tag;
     bool valid_value;
     bool dirty_value;
-}Cache_L2_Way;
+} L2;
 
-Cache_L2_Way cache_L2[Cache_L2_Set_Num * Cache_L2_Way_Num];
+L2 cache_L2[CACHE_L2_SET_NUM * CACHE_L2_WAY_NUM];
 
 void init_cache();
 
-int read_Cache_L1(hwaddr_t);
-int read_Cache_L2(hwaddr_t);
+int read_cache_L1(hwaddr_t);
+int read_cache_L2(hwaddr_t);
 
-void write_Cache_L1(hwaddr_t, size_t, uint32_t);
-void write_Cache_L2(hwaddr_t, size_t, uint32_t);
+void write_cache_L1(hwaddr_t, size_t, uint32_t);
+void write_cache_L2(hwaddr_t, size_t, uint32_t);
 #endif
