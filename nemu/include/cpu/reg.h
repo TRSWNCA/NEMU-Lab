3ac097eb32c8d27a92d507b9bb65ecd8acfc0e52
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-21 14:57:18 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:00:25 up  1:27,  0 users,  load average: 0.22, 0.07, 0.01 67afce22a5b11a3ebe60d07b6be34a211dccef7

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -7,6 +7,8 @@
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
+enum { R_ES, R_CS, R_SS, R_DS, R_FS, R_GS};
+
 
 /* TODO: Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
@@ -83,6 +85,8 @@ typedef struct {
 } CPU_state;
 
 extern CPU_state cpu;
+
+uint8_t current_sreg;
 
 static inline int check_reg_index(int index) {
   assert(index >= 0 && index < 8);
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -6,6 +6,13 @@
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
+lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg_id) {
+  if (cpu.cr0.protect_enable == 0) return addr;
+  else {
+    return cpu.sreg[sreg_id].base + addr;
+  }
+}
+
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
@@ -39,14 +46,16 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
 #ifdef DEBUG
-	assert(len == 1 || len == 2 || len == 4);
+  assert(len == 1 || len == 2 || len == 4);
 #endif
-	return lnaddr_read(addr, len);
+  lnaddr_t lnaddr = seg_translate(addr, len, current_sreg);
+  return lnaddr_read(lnaddr, len);
 }
 
 void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 #ifdef DEBUG
-	assert(len == 1 || len == 2 || len == 4);
+  assert(len == 1 || len == 2 || len == 4);
 #endif
-	lnaddr_write(addr, len, data);
+  lnaddr_t lnaddr = seg_translate(addr, len, current_sreg);
+  lnaddr_write(lnaddr, len, data);
 }
