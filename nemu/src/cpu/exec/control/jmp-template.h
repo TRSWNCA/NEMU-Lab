9c84f5fd9e642ee3c7c1832e4cca133c8373775f
tracer-TRSWNCA <trswnca@yeah.net>
2021-09-22 11:43:22 +0806

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:45:05 up 4 min,  0 users,  load average: 0.26, 0.12, 0.04 199d25eccd36bbe2128b1370eb35931a930403a5

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -16,4 +16,48 @@ make_helper(concat(jmp_rm_, SUFFIX)) {
   return len + 1;
 }
 
+#if DATA_BYTE == 4
+
+extern Sreg_Descriptor* sreg_desc;
+
+Sreg_Descriptor new_sreg_desc;
+
+make_helper(ljmp) {
+  sreg_desc = &new_sreg_desc;
+
+  uint32_t op1 = instr_fetch(cpu.eip + 1, 4);
+  uint16_t op2 = instr_fetch(cpu.eip + 1 + 4, 2);
+
+  cpu.eip = op1 - 7;
+  cpu.cs.selector = op2;
+
+  uint16_t idx = cpu.cs.selector >> 3;
+
+  Assert((idx << 3) <= cpu.gdtr.limit, "Segement Selector Is Out Of The Limit!");
+
+  uint32_t chart_addr = cpu.gdtr.base + (idx << 3);
+
+  sreg_desc -> part1 = lnaddr_read(chart_addr, 4);
+  sreg_desc -> part2 = lnaddr_read(chart_addr + 4, 4);
+
+  Assert(sreg_desc -> p == 1, "Segement Not Exist!");
+  uint32_t bases = 0;
+  bases |= ((uint32_t)sreg_desc -> base1);
+  bases |= ((uint32_t)sreg_desc -> base2) << 16;
+  bases |= ((uint32_t)sreg_desc -> base3) << 24;
+  cpu.cs.base = bases;
+  uint32_t limits = 0;
+  limits |= ((uint32_t)sreg_desc -> limit1);
+  limits |= ((uint32_t)sreg_desc -> limit2) << 16;
+  limits |= ((uint32_t)0xfff) << 24;
+  cpu.cs.limit = limits;
+
+  if (sreg_desc -> g == 1) {
+    cpu.cs.limit <<= 12;
+  }
+  print_asm("ljump %x %x", op2, op1);
+  return 1 + 6;
+}
+#endif
+
 #include "cpu/exec/template-end.h"
