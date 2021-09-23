1dad1126b17981800605d8d55d0af8b00be7ff2a
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-23 11:06:52 +0800

    >  compile NEMU 3020244141 trswnca Linux LAPTOP-5DSBBEQG 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:06:52 up  1:34,  0 users,  load average: 0.27, 0.13, 0.11 fc3a57111e42c084c86a26932663d2455c9fd42c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,6 +13,31 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg_id) {
   }
 }
 
+hwaddr_t page_translate(lnaddr_t addr) {
+  if (cpu.cr0.protect_enable == 1 && cpu.cr0.paging == 1) {
+    uint32_t dir = addr >> 22;
+    uint32_t page = (addr >> 12) & 0x3ff;
+    uint32_t bias = addr & 0xfff;
+
+    uint32_t dir_start = cpu.cr3.page_directory_base;
+    uint32_t dir_position = (dir_start << 12) + (dir << 2);
+    Page_Descriptor first_content;
+    first_content.val = hwaddr_read(dir_position, 4);
+    Assert(first_content.p == 1, "Page unavailable");
+    uint32_t page_start = first_content.addr;
+    uint32_t page_pos = (page_start << 12) + (page << 2);
+    Page_Descriptor second_content;
+    second_content.val =  hwaddr_read(page_pos, 4);
+    Assert(second_content.p == 1, "Page unavailable");
+    uint32_t addr_start = second_content.addr;
+    hwaddr_t hwaddr = (addr_start << 12) + bias;
+    return hwaddr;
+  } else {
+    return addr;
+  }
+}
+
+
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
@@ -37,11 +62,25 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
-	return hwaddr_read(addr, len);
+  assert(len == 1 || len == 2 || len == 4);
+  uint32_t cur_bias = addr & 0xfff;
+  if (cur_bias + len - 1 > 0xfff) {
+    assert(0);
+  } else {
+    hwaddr_t hwaddr = page_translate(addr);
+    return hwaddr_read(hwaddr, len);
+  }
 }
 
 void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
-	hwaddr_write(addr, len, data);
+  assert(len == 1 || len == 2 || len == 4);
+  uint32_t cur_bias = addr & 0xfff;
+  if (cur_bias + len - 1 > 0xfff) {
+    assert(0);
+  } else {
+    hwaddr_t hwaddr = page_translate(addr);
+    hwaddr_write(hwaddr, len, data);
+  }
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
